75cb92b5f02b3c1b8907ce60916de57981fc68b9
MischaGontar <turictscol40@gmail.com>
2021-09-08 13:52:12 +0300

    solve the problem add script for generate table

---
+++ Page/components/js/table-script.js
@@ -0,0 +1,29 @@
+const lesson = (title, photo, data, trainer) => {
+    return {
+        title: title,
+        photoUrl: `./../photos/${photo}_photo.png`,
+        data: data,
+        trainer: trainer
+    }
+}
+
+const lessons = [
+    lesson('Баскетбол', 'basketball', "17:00 (08.09.2021)", "Traktor I.V."),
+    lesson('Плавання', 'swimming', "14:20 (08.09.2021)", "Traktor I.V."),
+    lesson('Волейбол', 'volleyball', "13:10 (08.09.2021)", "Traktor I.V.")
+]
+const table = document.getElementById('table')
+for (let i of lessons) {
+    table.innerHTML +=
+        `<tr>
+            <th> ${i.title} </th>
+            <th> <img alt="..." src="${i.photoUrl}" style="width: 150px;height: 100px"> </th>
+            <th> ${i.data} </th>
+            <th> Тренер: ${i.trainer} </th>
+            <th>
+                <form action="./../System/Error.html" method="get">
+                    <input type="submit" value="Записатися">
+                </form>
+           </th>
+        </tr>`
+}
