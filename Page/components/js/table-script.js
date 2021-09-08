99d28d91b11fbc1226b360417b3d18ad02568b97
MischaGontar <turictscol40@gmail.com>
2021-09-08 14:40:26 +0300

    add photos and generate random data

--- Page/components/js/table-script.js
+++ Page/components/js/table-script.js
@@ -1,17 +1,40 @@
-const lesson = (title, photo, data, trainer) => {
+const lesson = (title, photo, trainer) => {
+    function getRandom(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min; // max & min both included
+    }
+    function generateData(){
+        const hour = getRandom(0,23)
+        const time = getRandom(0,59)
+        const day = getRandom(10,28)
+        const month = getRandom(9,12)
+
+        return `час: ${(hour<10)?`0${hour}`: hour}:${(time<10)?`0${time}`: time} 
+                дата:${(day<10)?`0${day}`: day}.${(month<10)?`0${month}`: month}.2021`
+    }
     return {
         title: title,
         photoUrl: `./../photos/${photo}_photo.png`,
-        data: data,
+        data: generateData(),
         trainer: trainer
     }
 }
 
 // Буде винесено в БД
 const lessons = [
-    lesson('Баскетбол', 'basketball', "17:00 (08.09.2021)", "Traktor I.V."),
-    lesson('Плавання', 'swimming', "14:20 (08.09.2021)", "Traktor I.V."),
-    lesson('Волейбол', 'volleyball', "13:10 (08.09.2021)", "Traktor I.V.")
+    lesson('Американський футбол', 'americfootball', "Traktor I.V."),
+    lesson('Баскетбол', 'basketball', "Traktor I.V."),
+    lesson('Більярд', 'bilard', "Traktor I.V."),
+    lesson('Бокс', 'box', "Traktor I.V."),
+    lesson('Шахмати', 'chess', "Traktor I.V."),
+    lesson('Футбол', 'football', "Traktor I.V."),
+    lesson('Хокей', 'hokey', "Traktor I.V."),
+    lesson('Плавання', 'swimming', "Traktor I.V."),
+    lesson('Фехтування', 'swordplay', "Traktor I.V."),
+    lesson('Великий теніс', 'tennis', "Traktor I.V."),
+    lesson('Волейбол', 'volleyball', "Traktor I.V."),
+    lesson('Йога', 'yoga', "Traktor I.V.")
 ]
 
 const table = document.getElementById('table')
