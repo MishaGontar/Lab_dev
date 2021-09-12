d48581cbd05cd91c41b1ce316eeb18365f01f877
MischaGontar <turictscol40@gmail.com>
2021-09-12 23:16:55 +0300

    fix bug with url

--- Page/Controllers/LogOutController.php
+++ Page/Controllers/LogOutController.php
@@ -1,3 +1,3 @@
 <?php
 setcookie('i',null,-3600,path: '/');
-header("Location: http://localhost:63342/Lab_dev/PROJECT/Page/views/Home.php");
+header("Location: ./../views/Home.php");
--- Page/Controllers/LoginController.php
+++ Page/Controllers/LoginController.php
@@ -8,7 +8,7 @@ $password = $_POST["password"];
 $id_user = $data->LoginCheck("$username", "$password");
 
 if (!is_null($id_user)) {
-    header("Location: http://localhost:63342/Lab_dev/PROJECT/Page/views/Home.php");
+    header("Location: ./../views/Home.php");
 } else {
 include_once './../views_models/header.php' ;
     echo "<h1>Login isn`t correct</h1><br> Input Username: " . $username . "<br> Input Password: " . $password."<br>";
